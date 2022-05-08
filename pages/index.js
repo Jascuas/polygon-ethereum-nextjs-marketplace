c2e335eb1049216b8ccda9cc74a9bb7bc3ddef14
Jascuas <loljavierdelago@hotmail.com>
2022-05-08 20:35:34 +0200

    artifacts

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
     */
     const items = await Promise.all(data.map(async i => {
       const tokenUri = await contract.tokenURI(i.tokenId)
-      const meta = await axios.get(tokenUri, {withCredentials: false})
+      const meta = await axios.get(tokenUri)
       let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
       let item = {
         price,
